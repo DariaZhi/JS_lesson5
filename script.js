6cc94d843ecd2a51ba3cf1731cb3d5937875a047
Daria <iamlucky.da@gmail.com>
2021-10-29 17:24:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // 1. На странице в html задать список элементов li с числами. Джававскриптом нужно выбрать из этого списка четные числа и создать из них массив. Затем, используя метод forEach, создать из массива новый список элементов li и поместить эти элементы в исходный тег ul. Для обхода через querySelectorAll использовать цикл for-of (forEach работать не будет)
 
 const liList = [...document.querySelectorAll('li')];
+const ulList = document.querySelector('ul');
 
 const liListWithEven = liList.filter((item) => {
     return item.innerHTML % 2 == 0
 }
 )
 
-for (let item of liListWithEven) {
-const newLiList = document.createElement('li');
-newLiList.innerHTML = item;
-document.querySelector('ul').append(newLiList)
-}
+liListWithEven.forEach((item) => ulList.append(item));
 
 
 
